bce3b1303914eef9a80569880457c5a4934b3a4f
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-07-19 05:37:42 +0300

    prompt the user to enter the sorting algorithm

--- main.go
+++ main.go
@@ -2,14 +2,38 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"github.com/manifoldco/promptui"
 )
 
 func main() {
 	arr := generateRandomArray(10)
 	fmt.Println("Initial array:", arr)
-	time.Sleep(time.Second) // Pause for visualization
+
+	// Prompt user to select the sorting algorithm
+	prompt := promptui.Select{
+		Label: "Select Sorting Algorithm",
+		Items: []string{"Bubble Sort", "Selection Sort", "Merge Sort"},
+	}
+
+	_, algorithm, err := prompt.Run()
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
 	clearConsole()
-	bubbleSortVisualizer(arr)
+
+	switch algorithm {
+	case "Bubble Sort":
+		bubbleSortVisualizer(arr)
+	case "Selection Sort":
+		// selectionSortVisualizer(arr)
+	case "Merge Sort":
+		// mergeSortVisualizer(arr)
+	default:
+		fmt.Println("Invalid selection")
+		return
+	}
+
 	fmt.Println("Sorted array:", arr)
 }
