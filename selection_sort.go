bb79797d67dd94e224f58557ee562d3d78ce95ca
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-07-21 14:03:05 +0300

    use visualizeArray util func

--- selection_sort.go
+++ selection_sort.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"time"
 )
 
@@ -9,20 +8,13 @@ func selectionSortVisualizer(arr []int, delay time.Duration) {
 	for i := 0; i < len(arr); i++ {
 		min := i
 		for j := i + 1; j < len(arr); j++ {
-			printColoLightYellowArray(White, arr, i, j)
-			fmt.Printf(" // min = %d\n", arr[min])
-			time.Sleep(delay)
-			clearConsole()
+			visualizeIteration(White, arr, i, j, delay)
 			if arr[j] < arr[min] {
 				visualizeIteration(LightYellow, arr, i, j, delay)
 				min = j
 			}
 		}
-		swapStr := fmt.Sprintf(" // swap arr[i]: %d and arr[min]: %d\n", arr[i], arr[min])
 		arr[i], arr[min] = arr[min], arr[i]
-		printColoLightYellowArray(LightBlue, arr, i, min)
-		fmt.Print(swapStr)
-		time.Sleep(delay)
-		clearConsole()
+		visualizeIteration(LightBlue, arr, i, min, delay)
 	}
 }
